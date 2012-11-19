cdcf9cb23d9bbf877199943fa570e93b29e25f0b
Casey Sybrandy <casey.sybrandy@six3systems.com>
2012-11-19 14:59:43 -0500

    Redis commands are now parsed more correctly.
    A "fast path" is still there in case there are now quotes, but now if there
    are quotes, then the correct output is created.  Have not tested nested quotes
    yet.

--- redis_proto/redis_proto.d
+++ redis_proto/redis_proto.d
@@ -6,19 +6,20 @@ import std.string;
 
 string toRedisProto(string command)
 {
-    string[] args = split(command);
     auto proto = appender!(string)();
 
-    if (args[2].indexOf("\"") == 0)
+    if (command.indexOf("\"") > 0)
     {
-        formattedWrite(proto, "*%d\r\n", 3);
-        formattedWrite(proto, "$%d\r\n%s\r\n", args[0].length, args[0]);
-        formattedWrite(proto, "$%d\r\n%s\r\n", args[1].length, args[1]);
-        string value = command[command.indexOf("\"")..$];
-        formattedWrite(proto, "$%d\r\n%s\r\n", value.length, value);
+        string[] parts = parseString(command);
+        formattedWrite(proto, "*%d\r\n", parts.length);
+        for (int i = 0; i < parts.length; i++)
+        {
+            formattedWrite(proto, "$%d\r\n%s\r\n", parts[i].length, parts[i]);
+        }
     }
     else
     {
+        string[] args = split(command);
         formattedWrite(proto, "*%d\r\n", args.length);
 
         foreach (string arg; args)
@@ -27,13 +28,48 @@ string toRedisProto(string command)
         }
     }
 
+    debug(testing) { writeln("Data: ", proto.data); }
     return proto.data;
+}
+
+string[] parseString(string command)
+{
+    string[] parts;
+    char[] collector;
+    bool inQuotes = false;
+    foreach (char ch; command)
+    {
+        if (ch == '\"' && !inQuotes)
+        {
+            inQuotes = true;
+        }
+        else if (ch == '\"' && inQuotes)
+        {
+            inQuotes = false;
+        }
+        else if (!inQuotes && ch == ' ')
+        {
+            parts ~= collector.idup;
+            collector = [];
+        }
+        else
+        {
+            collector ~= ch;
+        }
+    }
+    parts ~= collector.idup;
+    debug(testing) { writeln("current parts: ", parts); }
+    return parts;
 }
 
 unittest
 {
-    assert(toRedisProto(q"[SET key value]") == "*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n");
-    assert(toRedisProto(q"[SET key "some value"]") == "*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$12\r\n\"some value\"\r\n");
+    assert(toRedisProto(q"[SET key value]") ==
+           "*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n");
+    assert(toRedisProto(q"[SET key "some value"]") ==
+           "*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$10\r\nsome value\r\n");
+    assert(toRedisProto(q"[HMSET hashkey first "value" second "value"]") ==
+           "*6\r\n$5\r\nHMSET\r\n$7\r\nhashkey\r\n$5\r\nfirst\r\n$5\r\nvalue\r\n$6\r\nsecond\r\n$5\r\nvalue\r\n");
 }
 
 void main()
